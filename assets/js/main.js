d598f3683dec044eea65fa27ef4c9ff22a245616
minnafeng <minnafeng02@gmail.com>
2022-07-27 02:18:05 -0400

    Update files

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,6 +56,11 @@ navLink.forEach(n => n.addEventListener('click', linkAction)) // arrow function 
 
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-up class
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
